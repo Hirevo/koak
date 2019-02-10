8b933376c10a75d5b56fa6a323ee20c41c248ffa
Hirevo <nipolom@live.fr>
2019-02-10 22:00:52 +0100

    Minor changes in Codegen module

--- src/Codegen.hs
+++ src/Codegen.hs
@@ -2,47 +2,47 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Codegen where
 
-import LLVM.IRBuilder.Module as M
-import LLVM.IRBuilder.Monad as Mn
-import LLVM.IRBuilder.Constant as C
-import LLVM.AST.Constant as Cst
-import LLVM.AST.Global as Glb
-import LLVM.AST.Typed as Tpd
-import LLVM.AST.Linkage as Lnk
-import LLVM.IRBuilder.Instruction as I
-import LLVM.AST as AST
-import LLVM.AST.Type as T
+import qualified LLVM.IRBuilder.Module as M
+import qualified LLVM.IRBuilder.Monad as Mn
+import qualified LLVM.IRBuilder.Constant as C
+import qualified LLVM.AST.Constant as Cst
+import qualified LLVM.AST.Global as Glb
+import qualified LLVM.AST.Typed as Tpd
+import qualified LLVM.AST.Linkage as Lnk
+import qualified LLVM.IRBuilder.Instruction as I
+import qualified LLVM.AST as AST
+import qualified LLVM.AST.Type as T
 
 import Data.Char (ord)
 
-stringPtr :: MonadModuleBuilder m => String -> Name -> m Operand
+stringPtr :: M.MonadModuleBuilder m => String -> AST.Name -> m AST.Operand
 stringPtr str nm = do
     let asciiVals = map (fromIntegral . ord) str
         llvmVals  = map (Cst.Int 8) (asciiVals ++ [0])
-        char      = IntegerType 8
-        charStar  = ptr char
+        char      = T.IntegerType 8
+        charStar  = T.ptr char
         charArray = Cst.Array char llvmVals
         ty        = Tpd.typeOf charArray
-    emitDefn $ GlobalDefinition globalVariableDefaults {
-          name        = nm
-        , Glb.type'   = ty
-        , linkage     = Lnk.External
-        , isConstant  = True
-        , initializer = Just charArray
-        , unnamedAddr = Just GlobalAddr
+    M.emitDefn $ AST.GlobalDefinition Glb.globalVariableDefaults {
+          Glb.name        = nm
+        , Glb.type'       = ty
+        , Glb.linkage     = Lnk.External
+        , Glb.isConstant  = True
+        , Glb.initializer = Just charArray
+        , Glb.unnamedAddr = Just AST.GlobalAddr
     }
-    pure $ ConstantOperand $ Cst.BitCast (Cst.GlobalReference (ptr ty) nm) charStar
+    pure $ AST.ConstantOperand $ Cst.BitCast (Cst.GlobalReference (T.ptr ty) nm) charStar
 
 fib :: AST.Module
 fib = M.buildModule "bootstrap" $ mdo
     arr <- stringPtr "%d\n" "PRINT_STR"
-    printf <- M.extern (Name "printf") [T.ptr T.i8, T.i32] T.i32
-    fib <- M.function (Name "fibonnacci") [(T.i32, M.ParameterName "n")] T.i32 $ \[n] -> mdo
+    printf <- M.extern (AST.mkName "printf") [T.ptr T.i8, T.i32] T.i32
+    fib <- M.function (AST.mkName "fibonnacci") [(T.i32, M.ParameterName "n")] T.i32 $ \[n] -> mdo
         entry <- Mn.block `Mn.named` "entry"
         I.ret n
-    M.function (Name "main") [(T.i32, M.ParameterName "ac")] T.i32 $ \[ac] -> mdo
+    M.function (AST.mkName "main") [(T.i32, M.ParameterName "ac")] T.i32 $ \[ac] -> mdo
         entry <- Mn.block `Mn.named` "entry"
         fibVal <- I.call fib [(ac, [])]
         I.call printf [(arr, []), (fibVal, [])]
         zero <- C.int32 0
-        ret zero
+        I.ret zero
